d4ac380a787d52dc53f66772d562126bdc37e161
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-05 10:40:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
+    if (req.url == "/") res.statusCode = 304;
     console.log(req.method, req.url, "-", res.statusCode);
     res.send('Hello, Anistick Studio Online is currently in beta. i know that its dead lol, but i just felt like trying something new.')
 })
