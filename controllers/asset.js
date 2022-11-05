f1bc46bc8d55967c645f6765ab29df4f00f354bf
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-05 18:13:21 -0400

    Update asset.js

--- controllers/asset.js
+++ controllers/asset.js
@@ -1,17 +1,18 @@
-const loadPost = require("../request/post_body");
-const header = process.env.XML_HEADER;
-const formidable = require("formidable");
-const fUtil = require("../misc/file");
-const nodezip = require("node-zip");
-const base = Buffer.alloc(1, 0);
-const asset = require("./main");
-const http = require("http");
-const fs = require("fs");
-const starter = require("../starter/main");
-const util = require("../misc/util");
-const mp3Duration = require("mp3-duration");
-const tempfile = require("tempfile");
-const ffmpeg = require("fluent-ffmpeg");
+const express = require("express"),
+      router = express.Router(),
+      header = process.env.XML_HEADER,
+      formidable = require("formidable"),
+      fUtil = require("../models/file"),
+      nodezip = require("node-zip"),
+      base = Buffer.alloc(1, 0),
+      asset = require("../models/asset"),
+      http = require("http"),
+      fs = require("fs"),
+      starter = require("../models/starter"),
+      util = require("../models/util"),
+      mp3Duration = require("mp3-duration"),
+      tempfile = require("tempfile"),
+      ffmpeg = require("fluent-ffmpeg");
 ffmpeg.setFfmpegPath(require("@ffmpeg-installer/ffmpeg").path);
 ffmpeg.setFfprobePath(require("@ffprobe-installer/ffprobe").path);
 function movieXml(v) {
@@ -118,201 +119,163 @@ function convertVideoToFlv(ut, type, _ext, _buffer, subtype, filepath) {
  * @param {import("url").UrlWithParsedQuery} url
  * @returns {boolean}
  */
-module.exports = function (req, res, url) {	
-	switch (req.method) {
-		case "GET": {
-			const match = req.url.match(/\/assets\/([^/]+)\/([^/]+)$/);
-			if (!match) return;
-			const type = match[1];
-			const aId = match[2];
-			const dot = aId.lastIndexOf(".");
-			const ext = aId.substr(dot + 1);
-			asset.loadOnGetRequest(type, aId, ext).then(b => res.end(b)).catch(e => { 
-				console.log(e), res.end(`<center><h1>${e || "404 Not Found"}</h1></center>`)
-			});
-			return true;
-		} case "POST": {
-			switch (url.pathname) {
-				case "/goapi/getAsset/":
-				case "/goapi/getAssetEx/": {
-					loadPost(req, res).then(data => {
-						const [ prefix, id ] = data.assetId.split(".");
-						console.log(id.slice(0, -4));
-						asset.load(id.slice(0, -4)).then(b => {
-							res.setHeader("Content-Length", b.length);
-							res.setHeader("Content-Type", "audio/mp3");
-							res.end(b);
-						}).catch(e => {
-							res.end(1 + util.xmlFail(e));
-							console.log(e);
-							return;
-						});
-					}).catch(e => console.log(e));
-					return true;
-				} case "/goapi/updateAsset/": {
-					loadPost(req, res).then(data => {
-						const id = data.assetId.slice(0, -4);
-						const origTitle = fs.readFileSync(process.env.META_FOLDER + `/${id}-title.txt`);
-						const dot = origTitle.lastIndexOf(".") || "";
-						const ext = origTitle.subarray(dot + 1) || "";
-						if (!fs.existsSync(process.env.META_FOLDER + `/${id}-ext.txt`)) {
-							fs.writeFileSync(process.env.META_FOLDER + `/${id}-ext.txt`, ext);
-						}
-						fs.writeFileSync(process.env.META_FOLDER + `/${id}-title.txt`, data.title);
-					}).catch(e => console.log(e));
-					return true;
-				} case "/goapi/getUserAssets/": {	
-					loadPost(req, res).then(data => {
-						switch (data.type) {
-							case "movie": {
-								starter.list().then(async files => {
-									const xml = `${header}<ugc more="0">${files.map((v) => movieXml(v)).join('')}</ugc>`;
-									const zip = nodezip.create();
-									fUtil.addToZip(zip, "desc.xml", Buffer.from(xml));
-									res.setHeader("Content-Type", "application/zip");
-									res.write(base);
-									res.end(await zip.zip());
-								}).catch(e => {
-									res.end(1 + util.assetFail(e));
-									console.log(e);
-									return;
-								});
-								break;
-							} case "bg": {
-								function listBackgrounds() {
-									return new Promise(async res => {
-										const assets = asset.list("bg", "png");
-										files = asset.list("bg", "jpg");
-										const xml = `${header}<ugc more="0">${files.map((v) => backgroundXml(v)).join("")}${
-											assets.map((v) => backgroundXml(v)).join("")
-										}</ugc>`;
-										const zip = nodezip.create();
-										fUtil.addToZip(zip, "desc.xml", Buffer.from(xml));
-										res(await zip.zip());
-									});
-								}
-								listBackgrounds().then((buff) => {
-									res.setHeader("Content-Type", "application/zip");
-									res.write(base);
-									res.end(buff);
-								}).catch(e => {
-									res.end(1 + util.assetFail(e));
-									console.log(e);
-									return;
-								});
-								break;
-							} case "prop": {
-								function listProps() {
-									return new Promise(async res => {
-										files = asset.list("video", "flv");
-										const xml = `${header}<ugc more="0">${files.map((v) => videoXml(v)).join("")}</ugc>`;
-										const zip = nodezip.create();
-										fUtil.addToZip(zip, "desc.xml", Buffer.from(xml));
-										res(await zip.zip());
-									});
-								}
-								listProps().then((buff) => {
-									res.setHeader("Content-Type", "application/zip");
-									res.write(base);
-									res.end(buff);
-								}).catch(e => {
-									res.end(1 + util.assetFail(e));
-									console.log(e);
-									return;
-								});
-								break;
-							}
-						}
-					}).catch(e => console.log(e));
-					return true;
-				} case "/goapi/getUserAssetsXml/": {
-					loadPost(req, res).then(data => listAssets(data)).then((buff) => {
-						res.setHeader("Content-Type", "text/xml");
-						res.end(buff);
-					}).catch(e => {
-						res.end(1 + util.assetFail(e));
-						console.log(e);
-						return;
+router.get(/\/assets\/([^/]+)\/([^/]+)$/, (req, res) => {
+	const type = req.matches[1];
+	const aId = req.matches[2];
+	const dot = aId.lastIndexOf(".");
+	const ext = aId.substr(dot + 1);
+	asset.loadOnGetRequest(type, aId, ext).then(b => res.end(b)).catch(e => { 
+		console.log(e), res.end(`<center><h1>${e || "404 Not Found"}</h1></center>`)
+	});
+})
+router.post(["/goapi/getAsset/","/goapi/getAssetEx/"], (req, res) => {
+	new formidable.IncomingForm().parse(req, (e, f) => {
+		const [ prefix, id ] = f.assetId.split(".");
+		console.log(id.slice(0, -4));
+		asset.load(id.slice(0, -4)).then(b => {
+			res.setHeader("Content-Length", b.length);
+			res.setHeader("Content-Type", "audio/mp3");
+			res.end(b);
+		}).catch(e => { res.end(1 + util.xmlFail(e)), console.log(e) });
+	});
+})
+router.post("/goapi/updateAsset/", (req, res) => {
+	new formidable.IncomingForm().parse(req, (e, f) => {
+		const id = f.assetId.slice(0, -4);
+		const origTitle = fs.readFileSync(process.env.META_FOLDER + `/${id}-title.txt`);
+		const dot = origTitle.lastIndexOf(".") || "";
+		const ext = origTitle.subarray(dot + 1) || "";
+		if (!fs.existsSync(process.env.META_FOLDER + `/${id}-ext.txt`)) fs.writeFileSync(process.env.META_FOLDER + `/${id}-ext.txt`, ext);
+		fs.writeFileSync(process.env.META_FOLDER + `/${id}-title.txt`, data.title);
+	});
+})
+router.post("/goapi/getUserAssets/", (req, res) => {
+	new formidable.IncomingForm().parse(req, (e, f) => {
+		switch (f.type) {
+			case "movie": {
+				starter.list().then(async files => {
+					const xml = `${header}<ugc more="0">${files.map((v) => movieXml(v)).join('')}</ugc>`;
+					const zip = nodezip.create();
+					fUtil.addToZip(zip, "desc.xml", Buffer.from(xml));
+					res.setHeader("Content-Type", "application/zip");
+					res.write(base);
+					res.end(await zip.zip());
+				}).catch(e => { res.end(1 + util.assetFail(e)), console.log(e) });
+				break;
+			} case "bg": {
+				function listBackgrounds() {
+					return new Promise(async res => {
+						const assets = asset.list("bg", "png");
+						files = asset.list("bg", "jpg");
+						const xml = `${header}<ugc more="0">${files.map((v) => backgroundXml(v)).join("")}${assets.map((v) => backgroundXml(v)).join("")}</ugc>`;
+						const zip = nodezip.create();
+						fUtil.addToZip(zip, "desc.xml", Buffer.from(xml));
+						res(await zip.zip());
 					});
-					return true;
-				} case "/upload_asset": {
-					formidable().parse(req, (_, fields, files) => {
-						var [ ut, type, ext ] = fields.params.split(".");
-						var subtype = "",
-						prefix = "";
-						const path = files.import.path;
-						const name = files.import.name;
-						var buffer = fs.readFileSync(path);
-						switch (type) {
-							case "vo":
-								type = "sound";
-								subtype = "voiceover";
-								prefix = "v";
-								break;
-							case "se":
-								type = "sound";
-								subtype = "soundeffect";
-								prefix = "s";
-								break;
-							case "mu":
-								type = "sound";
-								subtype = "bgmusic";
-								prefix = "bg";
-								break;
-							case "vi": {
-								type = "prop";
-								subtype = "video";
-								break;
-							}
-						}
-						if (subtype != "video") {
-							asset.save(ut, type, ext, buffer, subtype).then(id => {
-								asset.createMeta(id, name, type, subtype);
-								switch (type) {
-									case "prop": {
-										const meta = {
-											holdable: "0",
-											wearable: "0",
-											headable: "0",
-											placeable: "1"
-										};
-										fs.writeFileSync(process.env.META_FOLDER + `/p-${id}-meta.json`, JSON.stringify(meta));
-										break;
-									} case "sound": {
-										mp3Duration(buffer, (e, d) => {
-											if (e) {
-												console.log(e);
-												return;
-											}
-											fs.writeFileSync(process.env.META_FOLDER + `/${prefix}-${id}-dur.txt`, `dur.${d * 1e3}`);
-										});
-										break;
-									}
-								}
-							}).catch(e => console.log(e));
-							fs.unlinkSync(path);
-						} else convertVideoToFlv(ut, type, ext, buffer, subtype, path);
-						res.end();
-					});
-					return true;
-				} case "/goapi/deleteAsset/": {
-					loadPost(req, res).then(data => {
-						var type;
-						const id = data.assetId;
-						if (id.startsWith("p-")) type = "prop";
-						else if (id.startsWith("b-")) type = "bg";
-						else if (id.startsWith("bg-")) type = "bgmusic";
-						else if (id.startsWith("s-")) type = "soundeffect";
-						else if (id.startsWith("v-")) type = "voiceover";
-						else return;
-						asset.delete(id, type);
-					}).catch(e => {
-						console.log(e)
-						res.end(1 + util.xmlFail(e));
-					});
-					return true;
 				}
+				listBackgrounds().then((buff) => {
+					res.setHeader("Content-Type", "application/zip");
+					res.write(base);
+					res.end(buff);
+				}).catch(e => { res.end(1 + util.assetFail(e)), console.log(e) });
+				break;
+			} case "prop": {
+				function listProps() {
+					return new Promise(async res => {
+						files = asset.list("video", "flv");
+						const xml = `${header}<ugc more="0">${files.map((v) => videoXml(v)).join("")}</ugc>`;
+						const zip = nodezip.create();
+						fUtil.addToZip(zip, "desc.xml", Buffer.from(xml));
+						res(await zip.zip());
+					});
+				}
+				listProps().then((buff) => {
+					res.setHeader("Content-Type", "application/zip");
+					res.write(base);
+					res.end(buff);
+				}).catch(e => { res.end(1 + util.assetFail(e)), console.log(e; });
+				break;
 			}
-		} default: return;
-	}
-};
+		}
+	});
+})
+router.post("/goapi/getUserAssetsXml/", (req, res) => {
+	new formidable.IncomingForm().parse(req, (e, f) => listAssets(f)).then((buff) => {
+		res.setHeader("Content-Type", "text/xml");
+		res.end(buff);
+	}).catch(e => { res.end(1 + util.assetFail(e)), console.log(e) });
+})
+router.post("/upload_asset", (req, res) => {
+	formidable().parse(req, (_, fields, files) => {
+		var [ ut, type, ext ] = fields.params.split(".");
+		var subtype = "", prefix = "";
+		const path = files.import.path;
+		const name = files.import.name;
+		var buffer = fs.readFileSync(path);
+		switch (type) {
+			case "vo":
+				type = "sound";
+				subtype = "voiceover";
+				prefix = "v";
+				break;
+			case "se":
+				type = "sound";
+				subtype = "soundeffect";
+				prefix = "s";
+				break;
+			case "mu":
+				type = "sound";
+				subtype = "bgmusic";
+				prefix = "bg";
+				break;
+			case "vi": {
+				type = "prop";
+				subtype = "video";
+				break;
+			}
+		}
+		if (subtype != "video") {
+			asset.save(ut, type, ext, buffer, subtype).then(id => {
+				asset.createMeta(id, name, type, subtype);
+				switch (type) {
+					case "prop": {
+						const meta = {
+							holdable: "0",
+							wearable: "0",
+							headable: "0",
+							placeable: "1"
+						};
+						fs.writeFileSync(process.env.META_FOLDER + `/p-${id}-meta.json`, JSON.stringify(meta));
+						break;
+					} case "sound": {
+						mp3Duration(buffer, (e, d) => {
+							if (e) {
+								console.log(e);
+								return;
+							}
+							fs.writeFileSync(process.env.META_FOLDER + `/${prefix}-${id}-dur.txt`, `dur.${d * 1e3}`);
+						});
+						break;
+					}
+				}
+			}).catch(e => console.log(e));
+			fs.unlinkSync(path);
+		} else convertVideoToFlv(ut, type, ext, buffer, subtype, path);
+		res.end();
+	});
+})
+router.post("/goapi/deleteAsset/", (req, res) => {
+	new formidable.IncomingForm().parse(req, (e, f) => {
+		var type;
+		const id = f.assetId;
+		if (id.startsWith("p-")) type = "prop";
+		else if (id.startsWith("b-")) type = "bg";
+		else if (id.startsWith("bg-")) type = "bgmusic";
+		else if (id.startsWith("s-")) type = "soundeffect";
+		else if (id.startsWith("v-")) type = "voiceover";
+		else return;
+		asset.delete(id, type);
+	});
+})
+
+module.exports = router;
