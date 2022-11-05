fbbf0070baf482cdf3dd2dd2ae708f199119d719
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-05 10:30:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log(req.method, req.url, "-", res.statusCode);
-    res.send('Yo!')
+    res.send('Hello, Anistick Studio Online is currently in beta. i know that its dead lol, but i just felt like trying something new.')
 })
 app.listen(process.env.PORT || 3000)
