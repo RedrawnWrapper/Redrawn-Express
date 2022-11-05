b73db39307134dc4aab156937550ba633e7f5f12
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-05 19:41:50 -0400

    Update character.js

--- models/character.js
+++ models/character.js
@@ -66,129 +66,95 @@ function getThumbPath(id) {
 	}
 }
 
-module.exports = {
-	/**
-	 * @param {string} id
-	 * @returns {Promise<string>}
-	 */
-	getTheme(id) {
-		return new Promise((res, rej) => {
-			if (themes[id]) res(themes[id]);
-			this.load(id)
-				.then((b) => res(addTheme(id, b)))
-				.catch(rej);
-		});
-	},
-	/**
-	 * @param {string} id
-	 * @returns {Promise<Buffer>}
-	 */
-	load(id) {
-		return new Promise((res, rej) => {
-			var i = id.indexOf("-");
-			var prefix = id.substr(0, i);
-			var suffix = id.substr(i + 1);
+exports.getTheme = function(id) {
+	return new Promise((res, rej) => {
+		if (themes[id]) res(themes[id]);
+		else this.load(id).then((b) => res(addTheme(id, b))).catch(e => rej(e));
+	});
+};
+exports.load = function(id) {
+	return new Promise((res, rej) => {
+		var i = id.indexOf("-");
+		var prefix = id.substr(0, i);
+		var suffix = id.substr(i + 1);
 
+		switch (prefix) {
+			case "c":
+			case "C":
+				fs.readFile(getCharPath(id), (e, b) => {
+					if (e) {
+						var fXml = util.xmlFail();
+						rej(Buffer.from(fXml));
+					} else res(b);
+				});
+				break;
+
+			case "":
+			default: {
+				// Blank prefix is left here for backwards-compatibility purposes.
+				var nId = Number.parseInt(suffix);
+				var xmlSubId = nId % fw;
+				var fileId = nId - xmlSubId;
+				var lnNum = fUtil.padZero(xmlSubId, xNumWidth);
+				var url = `${baseUrl}/${fUtil.padZero(fileId)}.txt`;
+
+				get(url).then((b) => {
+					var line = b.toString("utf8").split("\n").find((v) => v.substr(0, xNumWidth) == lnNum);
+					if (line) res(Buffer.from(line.substr(xNumWidth)));
+					else rej("Error: Character Not Found");
+				}).catch((e) => rej(e));
+				break;
+			}
+		}
+	});
+};
+exports.loadStock = async function(cId) {
+	const nId = (cId.slice(0, -3) + "000").padStart(9, 0);
+	const chars = await get(`${baseUrl}/${nId}.txt`);
+	var line = chars.toString("utf8").split("\n").find(v => v.substring(0, 3) == cId.slice(-3));
+	if (line) return Buffer.from(line.substring(3));
+	else return Buffer.from("Character not found.");
+};
+exports.parseTheme = function(buffer) {
+	const beg = buffer.indexOf(`theme_id="`) + 10;
+	const end = buffer.indexOf(`"`, beg);
+	return buffer.slice(beg, end).toString();
+};
+exports.save = function(data, id) {
+	return new Promise((res, rej) => {
+		if (id) {
+			const i = id.indexOf("-");
+			const prefix = id.substr(0, i);
 			switch (prefix) {
 				case "c":
-				case "C":
-					fs.readFile(getCharPath(id), (e, b) => {
-						if (e) {
-							var fXml = util.xmlFail();
-							rej(Buffer.from(fXml));
-						} else {
-							res(b);
-						}
-					});
+				case "C": {
+					fs.writeFile(getCharPath(id), data, (e) => (e ? rej() : res(id)));
 					break;
-
-				case "":
-				default: {
-					// Blank prefix is left here for backwards-compatibility purposes.
-					var nId = Number.parseInt(suffix);
-					var xmlSubId = nId % fw;
-					var fileId = nId - xmlSubId;
-					var lnNum = fUtil.padZero(xmlSubId, xNumWidth);
-					var url = `${baseUrl}/${fUtil.padZero(fileId)}.txt`;
-
-					get(url)
-						.then((b) => {
-							var line = b
-								.toString("utf8")
-								.split("\n")
-								.find((v) => v.substr(0, xNumWidth) == lnNum);
-							if (line) {
-								res(Buffer.from(line.substr(xNumWidth)));
-							} else {
-								rej(Buffer.from(util.xmlFail()));
-							}
-						})
-						.catch((e) => rej(Buffer.from(util.xmlFail())));
+				} default: {
+					res(save(id, data));
+					break;
 				}
 			}
+		} else {
+			saveId = `c-${fUtil.getNextFileId("char-", ".xml")}`;
+			res(save(saveId, data));
+		}
+	});
+};
+exports.saveThumb = function(data, id) {
+	return new Promise((res, rej) => {
+		var thumb = Buffer.from(data, "base64");
+		fs.writeFileSync(getThumbPath(id), thumb);
+		res(id);
+	});
+};
+exports.loadThumb = function(id) {
+	return new Promise((res, rej) => {
+		fs.readFile(getThumbPath(id), (e, b) => {
+			if (e) {
+				var fXml = util.xmlFail();
+				rej(Buffer.from(fXml));
+			} else res(b);
 		});
-	},
-	async loadStock(cId) {
-		const nId = (cId.slice(0, -3) + "000").padStart(9, 0);
-		const chars = await get(`${baseUrl}/${nId}.txt`);
-		var line = chars.toString("utf8").split("\n").find(v => v.substring(0, 3) == cId.slice(-3));
-		if (line) return Buffer.from(line.substring(3));
-		else return Buffer.from("Character not found.");
-	},
-	parseTheme(buffer) {
-		const beg = buffer.indexOf(`theme_id="`) + 10;
-		const end = buffer.indexOf(`"`, beg);
-		return buffer.slice(beg, end).toString();
-	},
-	/**
-	 * @param {Buffer} data
-	 * @param {string} id
-	 * @returns {Promise<string>}
-	 */
-	save(data, id) {
-		return new Promise((res, rej) => {
-			if (id) {
-				const i = id.indexOf("-");
-				const prefix = id.substr(0, i);
-				switch (prefix) {
-					case "c":
-					case "C":
-						fs.writeFile(getCharPath(id), data, (e) => (e ? rej() : res(id)));
-					default:
-						res(save(id, data));
-				}
-			} else {
-				saveId = `c-${fUtil.getNextFileId("char-", ".xml")}`;
-				res(save(saveId, data));
-			}
-		});
-	},
-	/**
-	 * @param {Buffer} data
-	 * @param {string} id
-	 * @returns {Promise<string>}
-	 */
-	saveThumb(data, id) {
-		return new Promise((res, rej) => {
-			var thumb = Buffer.from(data, "base64");
-			fs.writeFileSync(getThumbPath(id), thumb);
-			res(id);
-		});
-	},
-	/**
-	 * @param {string} id
-	 * @returns {Promise<Buffer>}
-	 */
-	loadThumb(id) {
-		return new Promise((res, rej) => {
-			fs.readFile(getThumbPath(id), (e, b) => {
-				if (e) {
-					var fXml = util.xmlFail();
-					rej(Buffer.from(fXml));
-				} else {
-					res(b);
-				}
-			});
-		});
-	},
+	});
 };
