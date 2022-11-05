3e3205ca471e5164578392b75d56a3073d2f1740
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-05 12:14:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,16 +31,13 @@ Object.assign(process.env, env, require("./config"));
 
 // app functions
 app.use(require("./controllers"))
-app.all('/', (req, res) => {
-	try {
-		const p = url.parse(req.url, true);
-		console.log(req.method, p.path, "-", res.statusCode);
-		res.send('Hello, Anistick Studio Online is currently in beta. i know that its dead lol, but i just felt like trying something new.')
-	} catch (x) {
-		res.statusCode = 500;
-		const p = url.parse(req.url, true);
-		console.log(req.method, p.path, "-", res.statusCode);
-		res.send(x)
-	}
-})
+try {
+	const p = url.parse(req.url, true);
+	console.log(req.method, p.path, "-", res.statusCode);
+} catch (x) {
+	res.statusCode = 500;
+	const p = url.parse(req.url, true);
+	console.log(req.method, p.path, "-", res.statusCode);
+	res.send(x);
+}
 app.listen(process.env.PORT || 80)
