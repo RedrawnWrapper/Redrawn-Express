14f3e846802a577dc4365fcb6246570c488b4ad2
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-05 11:36:59 -0400

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -3,5 +3,11 @@ const express = require("express"),
 
 // configure routers
 router.use("/app", require("./page"))
+router.use('/', (req, res) => {
+    const p = url.parse(req.url, true);
+    if (req.url == "/") res.statusCode = 304;
+    console.log(req.method, p.path, "-", res.statusCode);
+    res.send('Hello, Anistick Studio Online is currently in beta. i know that its dead lol, but i just felt like trying something new.')
+})
 
 module.exports = router
