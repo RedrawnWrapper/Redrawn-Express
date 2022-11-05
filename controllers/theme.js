6ec9d7d59e396b946813e1fbc1f3ef537829d260
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-05 12:30:13 -0400

    Update theme.js

--- controllers/theme.js
+++ controllers/theme.js
@@ -13,10 +13,10 @@ const express = require("express"),
  */
 router.post("/getThemeList/", (req, res) => {
   res.setHeader("Content-Type", "application/zip");
-  fUtil.makeZip(`${folder}/_themelist.xml`, "themelist.xml").then((b) => res.send(b));
+  fUtil.makeZip(`${folder}/themelist.xml`, "themelist.xml").then((b) => res.send(b));
 })
 router.post("/getTheme/", (req, res) => {
-  loadPost(req, res).then(data => {
+	new formidable.IncomingForm().parse(req, (e, f) => {
 		res.setHeader("Content-Type", "application/zip");
 		fUtil.makeZip(`${folder}/${data.themeId}.xml`, "theme.xml").then((b) => res.end(b));
 	});
