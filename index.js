e72d3bae01d9dbfbe35a162f1f4a2a6eca920399
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-05 13:38:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,28 +4,6 @@ const app = express()
 const url = require("url")
 const fs = require("fs")
 
-// env stuff
-const env = {
-	CHAR_BASE_URL: "https://raw.githubusercontent.com/Anistick/anichars/master/characters",
-	THUMB_BASE_URL: "https://raw.githubusercontent.com/Anistick/anithumbs/master/thumbnails",
-	XML_HEADER: "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n",
-	CROSSDOMAIN: "<cross-domain-policy><allow-access-from domain=\"*\"/></cross-domain-policy>",
-	META_FOLDER: "./files/assets/meta",
-	FILE_WIDTH: 1000,
-	GATHER_THREADS: 100,
-	GATHER_THRESH1: 250000000,
-	GATHER_THRESH2: 328493000,
-	GATHER_THRESH3: 400000000,
-	FILE_NUM_WIDTH: 9,
-	XML_NUM_WIDTH: 3,
-	HTTP_PORT: 80,
-	HTTPS_PORT: 443,
-	NODE_ENV: "dev",
-	SAVED_FOLDER: "./files/saved",
-	CACHÉ_FOLDER: "./files/cache",
-	THEME_FOLDER: "./files/themes"
-};
-fs.writeFileSync("./env.json", JSON.stringify(env));
 Object.assign(process.env, require("./env"), require("./config"));
 
 // app functions
