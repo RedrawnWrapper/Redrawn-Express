8ac28477b6856cba1653fba1b3669d200c491e1f
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-05 12:19:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,13 +31,44 @@ Object.assign(process.env, env, require("./config"));
 
 // app functions
 app.use(require("./controllers"))
-try {
+const stuff = require("./models/page");
+app.use((req, res) => {
 	const p = url.parse(req.url, true);
-	console.log(req.method, p.path, "-", res.statusCode);
-} catch (x) {
-	res.statusCode = 500;
-	const p = url.parse(req.url, true);
-	console.log(req.method, p.path, "-", res.statusCode);
-	res.send(x);
-}
+	var methodLinks = stuff[req.method];
+	for (let linkIndex in methodLinks) {
+		var regex = new RegExp(linkIndex);
+		if (regex.test(p.path)) {
+			var t = methodLinks[linkIndex];
+			var link = t.regexLink ? p.path.replace(regex, t.regexLink) : t.link || p.path;
+			var headers = t.headers;
+			var path = `./${link}`;
+
+			try {
+				for (var headerName in headers || {}) {
+					res.setHeader(headerName, headers[headerName]);
+				}
+				res.statusCode = t.statusCode || 200;
+				if (t.content !== undefined) res.end(t.content);
+				else if (fs.existsSync(path)) {
+					if (t.contentReplace) {
+						content = fs.readFileSync(path, "utf8");
+						content = content.replace(/VERSIÖN/g, pjson.versionStr);
+						res.end(content);
+					} else fs.createReadStream(path).pipe(res);
+				} else throw null;
+			} catch (e) {
+				res.statusCode = t.statusCode || 404;
+				res.end();
+			}
+			return true;
+		}
+	}
+	try {
+		console.log(req.method, p.path, "-", res.statusCode);
+	} catch (x) {
+		res.statusCode = 500;
+		console.log(req.method, p.path, "-", res.statusCode);
+		res.send(x);
+	}
+})
 app.listen(process.env.PORT || 80)
