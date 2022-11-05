95d3a76a41395ec2e356c3b5be694871d13834bd
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-05 13:54:18 -0400

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,24 @@
 const express = require("express"),
-	router = express.Router()  
+	router = express.Router(),
+        url = require("url")
 
 // configure routers
 router.use("/app", require("./page"))
 router.use("/goapi", require("./theme"))
+
+// logs
+router.use((req, res) => {
+	try {
+		const p = url.parse(req.url, true);
+		if (req.url == "/") res.statusCode = 304;
+		console.log(req.method, p.path, "-", res.statusCode);
+	} catch (x) {
+		res.statusCode = 500;
+		const p = url.parse(req.url, true);
+		if (req.url == "/") res.statusCode = 304;
+		console.log(req.method, p.path, "-", res.statusCode);
+		res.send(x)
+	}
+})
 
 module.exports = router
