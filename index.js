3a041f5556e6ce33f67d4f7d5c3b80ad5a2f7744
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-05 10:38:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ app.all('/', (req, res) => {
     console.log(req.method, req.url, "-", res.statusCode);
     res.send('Hello, Anistick Studio Online is currently in beta. i know that its dead lol, but i just felt like trying something new.')
 })
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 80)
