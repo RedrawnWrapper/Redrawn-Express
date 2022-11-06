fa04dff6080b74625d280e516717e8c7286e6006
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-05 23:39:50 -0400

    Update asset.js

--- middlewares/asset.js
+++ middlewares/asset.js
@@ -1,6 +1,7 @@
 const thumbUrl = process.env.THUMB_BASE_URL;
-const get = require("../misc/get");
+const get = require("../models/get");
 const http = require("http");
+const asset = require("../models/asset");
 
 /**
  * @param {http.IncomingMessage} req
@@ -9,10 +10,20 @@ const http = require("http");
  * @returns {boolean}
  */
 module.exports = function (req, res, url) {
-  var path = url.pathname;
-	if (req.method != "GET" || !path.startsWith("/stock_thumbs")) return;
-	get(thumbUrl + path.substr(path.lastIndexOf("/"))).then((v) => res.end(v)).catch(e => {
-		console.log(e);
-	});
+	var path = url.pathname;
+	if (req.method != "GET") return; 
+	switch (path) {
+		case "/stock_thumbs": {
+			get(thumbUrl + path.substr(path.lastIndexOf("/"))).then((v) => res.end(v)).catch(e => console.log(e));
+			return true;
+		}
+	}
+	const match = req.url.match(/\/assets\/([^/]+)\/([^/]+)$/);
+	if (!match) return;
+	const type = match[1];
+	const aId = match[2];
+	const dot = aId.lastIndexOf(".");
+	const ext = aId.substr(dot + 1);
+	asset.loadOnGetRequest(type, aId, ext).then(b => res.end(b)).catch(e => { console.log(e), res.end(`<center><h1>${e || "404 Not Found"}</h1></center>`) });
 	return true;
 };
