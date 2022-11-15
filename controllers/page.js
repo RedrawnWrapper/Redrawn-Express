7d5e862e7cf32b88089a777b097c7fa476e4b12a
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-15 15:58:31 -0500

    Update page.js

--- controllers/page.js
+++ controllers/page.js
@@ -113,46 +113,55 @@ router.get("/cc_browser", (req, res) => {
 })
 router.get("/go_full", (req, res) => {
 	const p = url.parse(req.url, true);
-	const query = p.query;
-	title = "Video Editor";
-	attrs = {
-		data: process.env.SWF_URL + `/go_full${query.v || ""}.swf`,
-		type: "application/x-shockwave-flash",
-		width: "100%",
-		height: "100%",
-	};
-	params = {
-		flashvars: {
-			apiserver: "/",
-			storePath: process.env.STORE_URL + "/<store>",
-			isEmbed: 1,
-			ctc: "go",
-			ut: 50,
-			bs: "default",
-			appCode: "go",
-			page: "",
-			siteId: "go",
-			lid: 13,
-			isLogin: "Y",
-			retut: 1,
-			clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
-			themeId: "business",
-			tlang: "en_US",
-			movieId: query.movieId || "",
-			goteam_draft_only: 1,
-			isWide: 1,
-			collab: 0,
-			nextUrl: "/html/list.html",
-		},
-		allowScriptAccess: "always",
-	};
-	res.setHeader("Content-Type", "text/html; charset=UTF-8");
-	Object.assign(params.flashvars, query);
-	res.send(
-		`<script>document.title='${title}',flashvars=${JSON.stringify(
-			params.flashvars
-		)}</script><body style="margin:0px">${toObjectString(attrs, params)}</body>${stuff.pages["/go_full"] || ""}`
-	);
+	if (p.query.tray == "zimmertwins") {
+		var prefix;
+		if (req.headers.host == "localhost" && req.headers.host == `localhost:${process.env.port}`) prefix = "http";
+		else prefix = "https";
+		res.send(`<html><head><script>function redirect() { 
+		  location.href = 'https://zimmertwins.onrender.com/starters?homeUrl=${prefix}://${req.headers.host}/'; 
+		}</script></head><body onload="redirect()"></body></html>`);
+	} else {
+		const query = p.query;
+		title = "Video Editor";
+		attrs = {
+			data: process.env.SWF_URL + `/go_full${query.v || ""}.swf`,
+			type: "application/x-shockwave-flash",
+			width: "100%",
+			height: "100%",
+		};
+		params = {
+			flashvars: {
+				apiserver: "/",
+				storePath: process.env.STORE_URL + "/<store>",
+				isEmbed: 1,
+				ctc: "go",
+				ut: 50,
+				bs: "default",
+				appCode: "go",
+				page: "",
+				siteId: "go",
+				lid: 13,
+				isLogin: "Y",
+				retut: 1,
+				clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
+				themeId: "business",
+				tlang: "en_US",
+				movieId: query.movieId || "",
+				goteam_draft_only: 1,
+				isWide: 1,
+				collab: 0,
+				nextUrl: "/html/list.html",
+			},
+			allowScriptAccess: "always",
+		};
+		res.setHeader("Content-Type", "text/html; charset=UTF-8");
+		Object.assign(params.flashvars, query);
+		res.send(
+			`<script>document.title='${title}',flashvars=${JSON.stringify(
+				params.flashvars
+			)}</script><body style="margin:0px">${toObjectString(attrs, params)}</body>${stuff.pages["/go_full"] || ""}`
+		);
+	}
 })
 router.get("/player", (req, res) => {
 	const p = url.parse(req.url, true);
