a034af0b2712eabf7b1bfe6ae5bf5d18241878b1
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-15 23:05:31 -0500

    Delete theme.js

--- middlewares/theme.js
+++
@@ -1,9 +0,0 @@
-const fUtil = require("../models/file");
-const folder = process.env.THEME_FOLDER;
-
-module.exports = function(req, res, url) {
-  if (req.method == "POST" && url.pathname == "/goapi/getThemeList/?") return;
-  res.setHeader("Content-Type", "application/zip");
-  fUtil.makeZip(`${folder}/_themelist.xml`, "themelist.xml").then((b) => res.send(b)).catch(e => console.log(e));
-  return true;
-}
