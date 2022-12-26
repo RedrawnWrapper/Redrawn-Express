61769b0b0bf0ed9201580be1d3af21b56beb2467
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-12-26 12:38:30 -0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,3 @@
 @echo off
 if exist .git ( git pull || git add . && git stash && git pull )
-start oldrun.bat
-start anirun.bat
 if not exist node_modules ( npm install && npm start ) else ( npm start )
