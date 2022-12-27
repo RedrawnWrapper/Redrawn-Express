603c9990aff8de706139def78834f37aad615178
josephanimate2021 <jackiecrosman@gmail.com>
2022-12-27 01:38:24 -0800

    added restart just in case the app crashes (may most likely not work because it was untested)

--- run.bat
+++ run.bat
@@ -1,3 +1,7 @@
 @echo off
 if exist .git ( git pull || git add . && git stash && git pull )
-if not exist node_modules ( npm install && npm start ) else ( npm start )
+if not exist node_modules ( npm install && goto start ) else ( goto start )
+:start
+npm start && goto restart
+:restart
+goto start
