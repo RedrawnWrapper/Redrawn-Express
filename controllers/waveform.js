c6f4fdce4bf7df4af73ce336f88a92528b1afdda
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-12-27 18:54:39 -0800

    Update waveform.js

--- controllers/waveform.js
+++ controllers/waveform.js
@@ -6,12 +6,13 @@ const express = require("express"),
 
 router.post("/saveWaveForm/", (req, res) => {
 	new formidable.IncomingForm().parse(req, (e, f) => {
-    console.log(f);
+		const id = f.wfid.slice(0, -4);
+		fs.writeFileSync(process.env.ASSETS_FOLDER + `/${id}.wf`, f.waveform);
 	});
 })
 router.post("/getWaveForm/", (req, res) => {
 	new formidable.IncomingForm().parse(req, (e, f) => {
-    console.log(f);
+		console.log(f);
 	});
 })
 
