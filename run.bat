fce62957f8245b5a18ee4194eb1b575e06d6b888
josephanimate2021 <jackiecrosman@gmail.com>
2022-12-30 15:12:39 -0800

    bug fixes

--- run.bat
+++ run.bat
@@ -1,6 +1,3 @@
 @echo off
 if exist .git ( git pull || git add . && git stash && git pull )
-if not exist node_modules ( npm install && goto start ) else ( goto start )
-:start
-npm start
-goto start
+if not exist node_modules ( npm install && npm start ) else ( npm start )
