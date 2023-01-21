7a4b380e94a299685cb0cbce87acf6aa97eba048
Kia <kia@anistick.com>
2023-01-21 09:56:53 -0600

    FUCLER

--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,8 @@ const express = require("express"),
 // configure routers
 router.use("/app", require("./page"))
 router.use("/goapi", require("./theme"), require("./asset"), require("./movie"), require("./character"), require("./tts"), require("./waveform"))
-app.set('trust proxy', 1) // trust first proxy
-app.use(session({
+router.set('trust proxy', 1) // trust first proxy
+router.use(session({
   secret: 'fucler',
   resave: true,
   saveUninitialized: true,
